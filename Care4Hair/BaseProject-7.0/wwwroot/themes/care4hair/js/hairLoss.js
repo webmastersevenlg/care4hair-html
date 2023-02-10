d8427927876a22df88baac1bb5d1f067d8035997
yunierrodriguez <yunier.rodriguez@sevenlg.com>
2023-02-10 13:40:36 -0500

    Actualizando

---
+++ Care4Hair/BaseProject-7.0/wwwroot/themes/care4hair/js/hairLoss.js
@@ -0,0 +1,193 @@
+
+// Selector sec
+
+// Overview page
+let selector1 = document.querySelector('.selector-sec-item-1');
+let selector2 = document.querySelector('.selector-sec-item-2');
+let selectorText1 = document.querySelector('.selector-sec-text-1');
+let selectorText2 = document.querySelector('.selector-sec-text-2');
+
+function activateSelector1() {
+    selector1.classList.add('active');
+    selector2.classList.remove('active');
+
+    selectorText1.classList.add('active');
+    selectorText2.classList.remove('active');
+}
+function activateSelector2() {
+    selector2.classList.add('active');
+    selector1.classList.remove('active');
+
+    selectorText2.classList.add('active');
+    selectorText1.classList.remove('active');
+}
+
+// Causes page
+let selector3 = document.querySelector('.selector-sec-item-3');
+let selector4 = document.querySelector('.selector-sec-item-4');
+let selector5 = document.querySelector('.selector-sec-item-5');
+let selector6 = document.querySelector('.selector-sec-item-6');
+let selectorText3 = document.querySelector('.selector-sec-text-3');
+let selectorText4 = document.querySelector('.selector-sec-text-4');
+let selectorText5 = document.querySelector('.selector-sec-text-5');
+let selectorText6 = document.querySelector('.selector-sec-text-6');
+
+function activateSelector3() {
+    selector3.classList.add('active');
+    selector4.classList.remove('active');
+    selector5.classList.remove('active');
+    selector6.classList.remove('active');
+
+    selectorText3.classList.add('active');
+    selectorText4.classList.remove('active');
+    selectorText5.classList.remove('active');
+    selectorText6.classList.remove('active');
+}
+function activateSelector4() {
+    selector4.classList.add('active');
+    selector3.classList.remove('active');
+    selector5.classList.remove('active');
+    selector6.classList.remove('active');
+
+    selectorText4.classList.add('active');
+    selectorText3.classList.remove('active');
+    selectorText5.classList.remove('active');
+    selectorText6.classList.remove('active');
+}
+function activateSelector5() {
+    selector5.classList.add('active');
+    selector4.classList.remove('active');
+    selector3.classList.remove('active');
+    selector6.classList.remove('active');
+
+    selectorText5.classList.add('active');
+    selectorText4.classList.remove('active');
+    selectorText3.classList.remove('active');
+    selectorText6.classList.remove('active');
+}
+function activateSelector6() {
+    selector6.classList.add('active');
+    selector3.classList.remove('active');
+    selector5.classList.remove('active');
+    selector4.classList.remove('active');
+
+    selectorText6.classList.add('active');
+    selectorText3.classList.remove('active');
+    selectorText5.classList.remove('active');
+    selectorText4.classList.remove('active');
+}
+
+// Treatment page
+let selector7 = document.querySelector('.selector-sec-item-7');
+let selector8 = document.querySelector('.selector-sec-item-8');
+let selector9 = document.querySelector('.selector-sec-item-9');
+let selectorText7 = document.querySelector('.selector-sec-text-7');
+let selectorText8 = document.querySelector('.selector-sec-text-8');
+let selectorText9 = document.querySelector('.selector-sec-text-9');
+
+function activateSelector7() {
+    selector7.classList.add('active');
+    selector8.classList.remove('active');
+    selector9.classList.remove('active');
+
+    selectorText7.classList.add('active');
+    selectorText8.classList.remove('active');
+    selectorText9.classList.remove('active');
+}
+function activateSelector8() {
+    selector8.classList.add('active');
+    selector7.classList.remove('active');
+    selector9.classList.remove('active');
+
+    selectorText8.classList.add('active');
+    selectorText7.classList.remove('active');
+    selectorText9.classList.remove('active');
+}
+function activateSelector9() {
+    selector9.classList.add('active');
+    selector8.classList.remove('active');
+    selector7.classList.remove('active');
+
+    selectorText9.classList.add('active');
+    selectorText8.classList.remove('active');
+    selectorText7.classList.remove('active');
+}
+
+
+// Fue page
+let selector10 = document.querySelector('.selector-sec-item-10');
+let selector11 = document.querySelector('.selector-sec-item-11');
+let selector12 = document.querySelector('.selector-sec-item-12');
+let selector13 = document.querySelector('.selector-sec-item-13');
+let selectorText10 = document.querySelector('.selector-sec-text-10');
+let selectorText11 = document.querySelector('.selector-sec-text-11');
+let selectorText12 = document.querySelector('.selector-sec-text-12');
+let selectorText13 = document.querySelector('.selector-sec-text-13');
+
+function activateSelector10() {
+    selector10.classList.add('active');
+    selector11.classList.remove('active');
+    selector12.classList.remove('active');
+    selector13.classList.remove('active');
+
+    selectorText10.classList.add('active');
+    selectorText11.classList.remove('active');
+    selectorText12.classList.remove('active');
+    selectorText13.classList.remove('active');
+}
+function activateSelector11() {
+    selector11.classList.add('active');
+    selector10.classList.remove('active');
+    selector12.classList.remove('active');
+    selector13.classList.remove('active');
+
+    selectorText11.classList.add('active');
+    selectorText10.classList.remove('active');
+    selectorText12.classList.remove('active');
+    selectorText13.classList.remove('active');
+}
+function activateSelector12() {
+    selector12.classList.add('active');
+    selector11.classList.remove('active');
+    selector10.classList.remove('active');
+    selector13.classList.remove('active');
+
+    selectorText12.classList.add('active');
+    selectorText11.classList.remove('active');
+    selectorText10.classList.remove('active');
+    selectorText13.classList.remove('active');
+}
+function activateSelector13() {
+    selector13.classList.add('active');
+    selector10.classList.remove('active');
+    selector12.classList.remove('active');
+    selector11.classList.remove('active');
+
+    selectorText13.classList.add('active');
+    selectorText10.classList.remove('active');
+    selectorText12.classList.remove('active');
+    selectorText11.classList.remove('active');
+}
+
+
+// Male hair transplant page
+let selector14 = document.querySelector('.selector-sec-item-14');
+let selector15 = document.querySelector('.selector-sec-item-15');
+let selectorText14 = document.querySelector('.selector-sec-text-14');
+let selectorText15 = document.querySelector('.selector-sec-text-15');
+
+
+function activateSelector14() {
+    selector14.classList.add('active');
+    selector15.classList.remove('active');
+
+    selectorText14.classList.add('active');
+    selectorText15.classList.remove('active');
+}
+function activateSelector15() {
+    selector15.classList.add('active');
+    selector14.classList.remove('active');
+
+    selectorText15.classList.add('active');
+    selectorText14.classList.remove('active');
+}
